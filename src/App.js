ba571c305e626fa1a1995b60dbf3afe1b10d3a77
vyankovs <viktoriia.yankovska@gmail.com>
2020-11-09 11:22:15 +0100

    hashRouter

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import Home from './Components/Home';
 import Overview from './Components/Discover/Overview';
 
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <div className="main-container">
         <Switch>
           <Route exact path="/"><Home /> </Route>
           <Route path="/overview/:country"><Overview /> </Route>
         </Switch>
       </div>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
