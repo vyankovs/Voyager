f2c91f9f90bc6524bc42f6bf876da937c6968dfb
vyankovs <viktoriia.yankovska@gmail.com>
2020-11-09 11:23:27 +0100

    Updates

---
+++ precache-manifest.3d7456bcbc052bf9a957c6733e03e0c1.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd23d6f79c37a4b2aeef7b3918f58f8a",
+    "url": "/Voyager/index.html"
+  },
+  {
+    "revision": "b9dbf6ab0c5bed8b976a",
+    "url": "/Voyager/static/css/2.e5041b2a.chunk.css"
+  },
+  {
+    "revision": "048a203ce549c0ebeafb",
+    "url": "/Voyager/static/css/main.173a3cfe.chunk.css"
+  },
+  {
+    "revision": "b9dbf6ab0c5bed8b976a",
+    "url": "/Voyager/static/js/2.39b419fb.chunk.js"
+  },
+  {
+    "revision": "622fffac73e56111dcf3fea0a2e83cfb",
+    "url": "/Voyager/static/js/2.39b419fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "048a203ce549c0ebeafb",
+    "url": "/Voyager/static/js/main.6e724931.chunk.js"
+  },
+  {
+    "revision": "7ee0b9b9b9c802ac207e",
+    "url": "/Voyager/static/js/runtime-main.38d95472.js"
+  },
+  {
+    "revision": "4987e8ac4dc823e51fd62e1d510e0956",
+    "url": "/Voyager/static/media/Brazil.4987e8ac.jpg"
+  },
+  {
+    "revision": "96d56c4568fbf6c1956bc32797ad0631",
+    "url": "/Voyager/static/media/France.96d56c45.jpg"
+  },
+  {
+    "revision": "8fdbb3a701400acfdc8a0e7d97149c7b",
+    "url": "/Voyager/static/media/Japan_1920.8fdbb3a7.jpg"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/Voyager/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/Voyager/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "935e4756d1ae35e17a8daa1c5908bbdb",
+    "url": "/Voyager/static/media/brussels-1920.935e4756.jpg"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "/Voyager/static/media/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "/Voyager/static/media/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "/Voyager/static/media/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "ccfdb9dc442be0c629d331e94497428b",
+    "url": "/Voyager/static/media/fa-brands-400.ccfdb9dc.svg"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "/Voyager/static/media/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "05b53beb21e3ef13d28244545977152d",
+    "url": "/Voyager/static/media/fa-regular-400.05b53beb.woff"
+  },
+  {
+    "revision": "1a78af4105d4d56e6c34f76dc70bf1bc",
+    "url": "/Voyager/static/media/fa-regular-400.1a78af41.ttf"
+  },
+  {
+    "revision": "3a3398a6ef60fc64eacf45665958342e",
+    "url": "/Voyager/static/media/fa-regular-400.3a3398a6.woff2"
+  },
+  {
+    "revision": "ad3a7c0d77e09602f4ab73db3660ffd8",
+    "url": "/Voyager/static/media/fa-regular-400.ad3a7c0d.eot"
+  },
+  {
+    "revision": "e75dfd904d366a2560c63c23cfc98ef8",
+    "url": "/Voyager/static/media/fa-regular-400.e75dfd90.svg"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "/Voyager/static/media/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "/Voyager/static/media/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "/Voyager/static/media/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "/Voyager/static/media/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "/Voyager/static/media/fa-solid-900.ee09ad75.woff"
+  },
+  {
+    "revision": "79f9e07ea081ea14096541b2d80c2f09",
+    "url": "/Voyager/static/media/globe_PNG62.79f9e07e.png"
+  },
+  {
+    "revision": "b9c5579574d1e29d15d6310f39d75fff",
+    "url": "/Voyager/static/media/ukraine-kyiv_1920.b9c55795.jpg"
+  }
+]);
